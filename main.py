371f19fc250551d4aeca3635f43a7779282e75c5
WOUBLE <mail@mail.ru>
2025-11-21 15:15:00 +0700

    5

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from flask_sqlalchemy import SQLAlchemy
 import enum
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1@localhost:5432/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'MegaSecretKey'
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'index'
 
 class Role(enum.Enum):
     USER = "USER"
     MANAGER = "MANAGER"
     ADMIN = "ADMIN"
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     role = db.Column(db.Enum(Role), default=Role.USER, nullable=False)
+
+    def check_password(self, pwd):
+        print(self.password)
+        print(pwd)
+        return self.password == pwd
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -45,8 +56,24 @@ class PickupsPoint(db.Model):
     index = db.Column(db.Integer, unique=True, nullable=False)
     adress = db.Column(db.String(255), nullable=False)
 
-@app.route('/')
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            login_user(user)
+            flash('Успешный вход!','success')
+            return redirect(url_for('products'))
+        else:
+            flash('Неверный логин или пароль.', 'error')
     return render_template('index.html')
 
 @app.route('/users')
@@ -58,6 +85,13 @@ def users():
 def products():
     products = Product.query.all()
     return render_template('products.html', products=products)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Вы вышли из системы.', 'info')
+    return redirect(url_for('index'))
 
 @app.route('/create', methods=['GET', 'POST'])
 def create_user():
